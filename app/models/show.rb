5d4512bb8ca4f7eb2b030085b65ddf2559cd1e9c
Philip Roth <50246817+rothberry@users.noreply.github.com>
2019-07-08 11:31:52 -0500

    Done.

---
+++ app/models/show.rb
@@ -0,0 +1,37 @@
+require 'pry'
+class Show < ActiveRecord::Base
+
+    def self.highest_rating
+        # binding.pry
+        # highest_rating = 
+        Show.maximum("rating")
+        # Show.find_by(rating: highest_rating)
+    end
+
+    def self.most_popular_show
+        highest_rating = Show.maximum("rating")
+        Show.find_by(rating: highest_rating)
+    end
+    
+    def self.lowest_rating
+        Show.minimum("rating")
+    end
+    
+    def self.least_popular_show
+        lowest_rating = Show.minimum("rating")
+        Show.find_by(rating: lowest_rating)
+    end
+    
+    def self.ratings_sum
+        Show.sum("rating")
+    end
+    
+    def self.popular_shows
+        Show.where("rating >= 5")
+    end
+    
+    def self.shows_by_alphabetical_order
+        # binding.pry
+        Show.order("name asc")
+    end
+end
